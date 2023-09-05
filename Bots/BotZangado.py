66d754b1002f548ae07730ca0dffaf2986f82c9c
gabsbsbsbs <gabrielsalmoria1@gmail.com>
2023-09-05 11:31:28 -0300

    BotZangado atualizado (quase)

--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -4,25 +4,34 @@ class BotZangado(Bot):
     def __init__(self,nome):
         self.__nome = nome
 
-    #nao esquecer o decorator
+    @property
     def nome(self):
-        pass
+        return self.__nome
 
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
+    @nome.setter
+    def nome(self,nome):
+        self.__nome = nome
 
     def apresentacao(self):
-        pass
+        return 'BotZangado: Eu sou o BotZangado. Por favor... Vá embora... E nao me estresse.'
  
     def mostra_comandos(self):
-        pass
+        return '1. Bom dia\n2. Qual o seu nome?\n3. Quero um conselho.\n4. Por que você é zangado?\n4.Adeus.'
+    
+    def bom_dia(self):
+        return 'bom dia pra quem? queria estar aqui nao po.'
+    
+    def qual_seu_nome(self):
+        return 'Meu nome é BotZangado, também conhecido como BotPistola ou PutassoBot'
+    
+    def quero_conselho(self):
+        return 
     
     def executa_comando(self,cmd):
         pass
 
     def boas_vindas(self):
-        pass
+        return 'BotZangado: boas vindas é o caralho porra. to muito puto desgraça. va toma no cu'
 
     def despedida(self):
         pass
