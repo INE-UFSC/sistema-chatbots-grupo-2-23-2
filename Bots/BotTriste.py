5bcf2872baa9c84ae980ac4db376f88f3ec493a6
gabsbsbsbs <gabrielsalmoria1@gmail.com>
2023-09-10 23:05:17 -0300

    Adicionando BotZangado e BotTriste

---
+++ Bots/BotTriste.py
@@ -0,0 +1,49 @@
+from Bots.Bot import Bot
+
+class BotTriste(Bot):
+    def __init__(self,nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    def apresentacao(self):
+        return f'{self.__nome}: Eu sou o {self.__nome}. Por favor, não se incomode com a minha tristeza\nNão se incomode comigo na verdade.\nEu estou bem :c'
+ 
+    def mostra_comandos(self):
+        return '1. Bom dia\n2. Qual o seu nome?\n3. Quero um conselho.\n4. Por que você é triste?\n5.Adeus.'
+    
+    def bom_dia(self):
+        return f'{self.__nome}: É, até que o dia está sendo bom... \nSó tenho 10 listas de cálculo pra fazer...\nNem chorei hoje ainda...'
+    
+    def qual_seu_nome(self):
+        return f'{self.__nome}: Meu nome é {self.__nome}... Você pode me chamar de Tristonho se quiser...'
+    
+    def quero_conselho(self):
+        return f'{self.__nome}: Se um dia você quiser marcar um fut com os amigos, marque antes das 22h...\nNum dia que não está chovendo também... '
+    
+    def por_que_triste(self):
+        return f'{self.__nome}: Zerei a prova de calculo ontem, a morena me deixou semana passada e eu pisei num lego hoje de manhã... \nPior dia da minha vida'
+    
+    def executa_comando(self,cmd):
+        if cmd == 1:
+            print(self.bom_dia())
+        elif cmd == 2:
+            print(self.qual_seu_nome())
+        elif cmd == 3:
+            print(self.quero_conselho())
+        elif cmd == 4:
+            print(self.por_que_triste())
+        elif cmd == 5:
+            print(self.despedida())
+
+    def boas_vindas(self):
+        return f'{self.__nome}: Boas vindas! \nVocê quer ser meu amigo? \nEu não tenho nenhum amigo por enquanto :c'
+
+    def despedida(self):
+        return f'{self.__nome}: Não... não me deixe aqui... Eu achei que você era meu amigo...'
--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -13,25 +13,37 @@ class BotZangado(Bot):
         self.__nome = nome
 
     def apresentacao(self):
-        return 'BotZangado: Eu sou o BotZangado. Por favor... Vá embora... E nao me estresse.'
+        return f'{self.__nome}: Eu sou o {self.__nome}. Por favor... Vá embora... E nao me estresse.'
  
     def mostra_comandos(self):
-        return '1. Bom dia\n2. Qual o seu nome?\n3. Quero um conselho.\n4. Por que você é zangado?\n4.Adeus.'
+        return '1. Bom dia\n2. Qual o seu nome?\n3. Quero um conselho.\n4. Por que você é zangado?\n5.Adeus.'
     
     def bom_dia(self):
-        return 'bom dia pra quem? queria estar aqui nao po.'
+        return f'{self.__nome}: Bom dia pra QUEM? QUEM TA TENDO UM BOM DIA?\nQueria estar aqui nao po.'
     
     def qual_seu_nome(self):
-        return 'Meu nome é BotZangado, também conhecido como BotPistola ou PutassoBot'
+        return f'{self.__nome}:Meu nome é {self.__nome}, também conhecido como PutassoBot.\nTambém conhecido como quem não queria falar com você'
     
     def quero_conselho(self):
-        return 
+        return f'{self.__nome}: Time que está ganhando não se mexe.\nVocê estava ganhando antes de vir conversar comigo.'
+    
+    def por_que_zangado(self):
+        return f'{self.__nome}: MEUS AMIGOS SÓ MARCAM O FUT AS 22H DA NOITE DESGRAÇA...\nCOMO QUE EU SAIO DE CASA AS 22H NA CHUVA?????\nCOMO????'
     
     def executa_comando(self,cmd):
-        pass
+        if cmd == 1:
+            print(self.bom_dia())
+        elif cmd == 2:
+            print(self.qual_seu_nome())
+        elif cmd == 3:
+            print(self.quero_conselho())
+        elif cmd == 4:
+            print(self.por_que_zangado())
+        elif cmd == 5:
+            print(self.despedida())
 
     def boas_vindas(self):
-        return 'BotZangado: boas vindas é o caralho porra. to muito puto desgraça. va toma no cu'
+        return f'{self.__nome}: boas vindas é o CACETE. to muito PISTOLA. POR QUE VOCÊ VEM FALAR COMIGO??'
 
     def despedida(self):
-        pass
+        return f'Finalmente. Vai embora logo... Demorou demais já. TA OLHANDO O QUE AINDA VEI? VAI EMBORA.'
