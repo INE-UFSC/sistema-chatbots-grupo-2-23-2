bdbc61862ee418973e5ac7f231c6348edd7e6d33
LauraQueiroz <72163686+LauraQueiroz@users.noreply.github.com>
2023-09-12 09:29:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
+from Bots.BotTriste import BotTriste
+from Bots.BotFeliz import BotFeliz
+from Bots.BotGringo import BotGringo 
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+lista_bots = [BotZangado("Yoda"), BotTriste("Bolado"), BotFeliz("Smiley"), BotGringo("Kevin")]
 
 print("FALAROBOT™️")
 sys = scb.SistemaChatBot("FALAROBOT™️",lista_bots)
