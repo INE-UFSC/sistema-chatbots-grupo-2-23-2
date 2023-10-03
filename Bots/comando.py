ae261e4aab8f4a0e6a1d807e8217686455513138
gabsbsbsbs <gabrielsalmoria1@gmail.com>
2023-10-03 10:41:54 -0300

    Adicionando classe Comando

---
+++ Bots/comando.py
@@ -0,0 +1,25 @@
+import random
+
+class Comando:
+    def __init__(self, id, msg, respostas = []):
+        self.__id = id
+        self.__msg = msg
+        self.__respostas = respostas
+        
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def mensagem(self):
+        return self.__mensagem
+
+    def getRandomResposta(self):
+        return random.choice(self.__respostas)
+    
+    def addResposta(self, resposta):
+        self.__respostas.append(resposta)
+
+    def delResposta(self, resposta):
+        if resposta in self.__respostas:
+            del self.__respostas[resposta]
